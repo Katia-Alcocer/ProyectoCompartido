a155e2febc574c5ad93e0a8fca5ba8bf472e1e77
Katia-Alcocer <ki.alcoceraguilar@ugto.mx>
2025-06-01 21:41:45 -0600

    Datos para Cargar las tablas de Categorias,Provedores y Descuentos

---
+++ DatosExel.sql
@@ -0,0 +1,38 @@
+INSERT INTO Proveedores (Nombre)
+VALUES ('TRUPPER');
+
+
+INSERT INTO Descuentos (Categoria, Porcentaje) VALUES 
+('PrecioNormal', 0.00),
+('PUBLICO1', 0.00),
+('HERRERO2', 0.00),
+('HERRERO3', 0.00),
+('HERRERO4', 0.00),
+('MAYOREO1', 0.00),
+('MAYOREO2', 0.00),
+('LABB', 0.00);
+
+INSERT INTO Categorias (Nombre) VALUES
+('ANGULO'),
+('Angulo Lig'),
+('CUADRADO'),
+('REDONDO'),
+('TORCIDO'),
+('SOLERA'),
+('TEE'),
+('MONTEN'),
+('PTR'),
+('TUBO CED.30'),
+('TUBO CED.40'),
+('TUBO GALV'),
+('Base Tinaco'),
+('TUB. C20'),
+('TABLEROS'),
+('MOFLERO'),
+('T.INDUSTRIAL'),
+('TABULAR'),
+('TABULAR ZINTRO'),
+('TABULAR C.20'),
+('Espiral Redondo'),
+('Espiral Cuadrado');
+
